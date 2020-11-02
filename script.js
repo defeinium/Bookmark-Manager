baae27d453ff7a4c30d223594651aed3c102ba4d
defeinium <58759273+defeinium@users.noreply.github.com>
2020-11-02 15:02:17 +0800

    Adding new project

---
+++ script.js
@@ -0,0 +1,36 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.include('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+}
+// General Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
